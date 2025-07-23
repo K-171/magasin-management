9f921f446d8e332431ec70abdc172b6189e25106
K-171 <ilyass.kaouachi.biz@gmail.com>
2025-07-23 16:40:43 +0100

    Revert "Fix: Revert admin role name to 'admin' for clear log"
    This reverts commit ac70ccb19261a29b0970cd68e1292353d905150a.

--- app/api/movements/clear/route.ts
+++ app/api/movements/clear/route.ts
@@ -6,7 +6,7 @@ export async function DELETE() {
   const session = await getSession();
   console.log('User role:', session.role);
 
-  if (!session || session.role !== 'admin') {
+  if (!session || session.role !== 'Administrator') {
     return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
   }
 
